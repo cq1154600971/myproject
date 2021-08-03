c9ad79515a288dec2915d4ac09125c08a0591350
chenqiang <chenqiang.0609@bytedance.com>
2021-08-03 09:24:48 +0800

    test io
    Change-Id: Iea91645cf46a00d14d673c90910912789651aa24

--- test/main.go
+++ test/main.go
@@ -4,20 +4,41 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
+	"strconv"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	str := strings.ToLower(scanner.Text())
-	scanner.Scan()
-	ch := strings.ToLower(scanner.Text())
-	ans := 0
-	for i := range str {
-		if str[i] == ch[0] {
-			ans++
+	//scanner := bufio.NewScanner(os.Stdin)
+	//scanner.Scan()
+	//str := strings.ToLower(scanner.Text())
+	//scanner.Scan()
+	//ch := strings.ToLower(scanner.Text())
+	//ans := 0
+	//for i := range str {
+	//	if str[i] == ch[0] {
+	//		ans++
+	//	}
+	//}
+	//fmt.Printf("%d\n", ans)
+	scan := bufio.NewScanner(os.Stdin)
+	for {
+		scan.Scan()
+		if len(scan.Text()) == 0 {
+			break
+		}
+		count,_ := strconv.Atoi(scan.Text())
+		n := [1001]bool{}
+		for i := 0;i < count;i++ {
+			scan.Scan()
+			numstr := scan.Text()
+			num,_ := strconv.Atoi(numstr)
+			n[num] = true
+		}
+
+		for i,v := range n {
+			if v {
+				fmt.Println(i)
+			}
 		}
 	}
-	fmt.Printf("%d\n", ans)
 }
